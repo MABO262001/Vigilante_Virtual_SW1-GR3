0aacc2b8165440ffa71159281ac61ceb3a6d2a0a
IngePapitas <osinagax10@gmail.com>
2024-03-23 23:11:28 -0400

    Actualizacion en Panza

--- resources/views/VistaExamen/create.blade.php
+++ resources/views/VistaExamen/create.blade.php
@@ -5,15 +5,15 @@
 
 <div class="grid md:grid-cols-2 md:gap-x-2 mt-4">
 
-    <div class="p-2 md:border-r">
+    <div class="p-2 md:border-r pr-4">
         <div class="w-full">
-            <label for="" class="text-sm font-semibold block text-gray-700">Tema</label>
+            <label for="tema" class="text-sm font-semibold block text-gray-700">Tema</label>
             <input type="text" name="tema" id="tema" class="border-x-transparent border-t-transparent border-b focus:outline-none focus:border-transparent w-full ">
         </div>
 
         <div class="mt-4">
-            <label for="" class="text-sm font-semibold block text-gray-700">Descripcion</label>
-
+            <label for="descripcion" class="text-sm font-semibold block text-gray-700">Descripcion</label>
+            <input type="text" name="descripcion" id="descripcion" class="w-full h-[150px] rounded">
         </div>
     </div>
 
