4ef12bbc7a0821629af316c5d9cdccb2564a3716
MABO262001 <ballivian.miguel@ficct.uagrm.edu.bo>
2024-03-23 23:19:40 -0400

    Usuarios
    cuentas

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     {
         User::create([
             'id' => 1,
-            'name' => 'Miguel Angel Ballivian Ocampo',
+            'name' => 'MABO',
             'email' => 'ballivian02@gmail.com',
             'password' => Hash::make('123456789'),
         ]);
@@ -26,5 +26,30 @@ class UserSeeder extends Seeder
             'email' => 'papitas@gmail.com',
             'password' => Hash::make('1234'),
         ]);
+        User::create([
+            'id' => 3,
+            'name' => 'David',
+            'email' => 'davidchalarq@gmail.com',
+            'password' => Hash::make('123456789'),
+        ]);
+        User::create([
+            'id' => 4,
+            'name' => 'Rene',
+            'email' => 'renechungara03@gmail.com ',
+            'password' => Hash::make('123456789'),
+        ]);
+        User::create([
+            'id' => 5,
+            'name' => 'Julius',
+            'email' => 'juliogutierrezv15@gmail.com',
+            'password' => Hash::make('123456789'),
+        ]);
+        User::create([
+            'id' => 6,
+            'name' => 'Micakes',
+            'email' => 'micaelaorocag@gmail.com ',
+            'password' => Hash::make('123456789'),
+        ]);
+
     }
 }
